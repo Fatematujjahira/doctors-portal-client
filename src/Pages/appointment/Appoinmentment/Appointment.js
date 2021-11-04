f60558e771958818545c30647695b7221f4ad7e9
fatematujjahira <zohrafatema357@gmail.com>
2021-11-04 20:49:10 +0600

    dff

--- src/Pages/appointment/Appoinmentment/Appointment.js
+++ src/Pages/appointment/Appoinmentment/Appointment.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
+import AppoinmentHeader from '../appoinmentHeader/AppoinmentHeader';
+import AvailableAppionment from '../AvailableAppionment/AvailableAppionment';
 
 const appintment = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <AppoinmentHeader></AppoinmentHeader>
+            <AvailableAppionment></AvailableAppionment>
         </div>
     );
 };
--- src/Pages/appointment/appoinmentHeader/AppoinmentHeader.js
+++ src/Pages/appointment/appoinmentHeader/AppoinmentHeader.js
@@ -1,10 +1,18 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
-
+import chair from '../../../images/chair.png'
 const AppoinmentHeader = () => {
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    
+               </Grid>
+                <Grid item xs={12} md={6}>
+                   <img style={{width:'100%'}} src={chair} alt="" /> 
+               </Grid>
+            </Grid>
+       </Container>
     );
 };
 
